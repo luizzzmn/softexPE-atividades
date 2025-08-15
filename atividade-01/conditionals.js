627e5f5bde3c623fa9c4b528e23c130b3857d57e
luizzzmn <luizmanoel10098@gmail.com>
2025-08-15 19:32:02 -0300

    fix: separate files and add comments

---
+++ atividade-01/conditionals.js
@@ -0,0 +1,12 @@
+// atividade-01/conditionals.js
+// Exemplo de uso de condicionais em JavaScript
+let nota1Aluno = 8.5;
+let notaA2luno = 7.0;
+
+let media = (nota1Aluno + notaA2luno) / 2;
+
+if (media >= 7) {
+    console.log(`Aprovado com média ${media}`);
+} else {
+    console.log(`Reprovado com média ${media}`);
+}
--- atividade-01/loops.js
+++ atividade-01/loops.js
@@ -1,15 +1,6 @@
-
+// atividade-01/loops.js
+// Exemplo de uso de loops em JavaScript
 let alunos = ["João", "Maria", "Pedro", "Ana"];
-let nota1Aluno = 8.5;
-let notaA2luno = 7.0;
-
-let media = (nota1Aluno + notaA2luno) / 2;
-
-if (media >= 7) {
-    console.log(`Aprovado com média ${media}`);
-} else {
-    console.log(`Reprovado com média ${media}`);
-}
 
 for (let i = 0; i < alunos.length; i++) {
     console.log(`Aluno ${i + 1}: ${alunos[i]}`);
